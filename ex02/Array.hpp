29d3e3cd29d205153a0beb305710482bbba8c823
jpaul-kr <jonpk555@gmail.com>
2024-10-08 13:24:22 +0200

    ex02 done

--- ex02/Array.hpp
+++ ex02/Array.hpp
@@ -9,15 +9,53 @@ class Array
 {
 	private:
 		T*	array;
-		size_t	len;
+		int	len;
 	public:
-		Array();
-		Array(unsigned int n);
-		~Array();
-		Array&	operator=(const Array& cpy);
-		T&	operator[](const int index);
+		Array() : len(0)
+		{
+			this->array = new T[0];
+		}
 
-		size_t	size();
+		Array(unsigned int n) : len(n)
+		{
+			this->array = new T[n];
+		}
+
+		Array(const Array& cpy)
+		{
+			this->len = cpy.len;
+			this->array = new T[cpy.len];
+			for (int i = 0; i < cpy.len; i++)
+				this->array[i] = cpy.array[i];
+		}
+
+		~Array()
+		{
+			delete [] this->array;
+		}
+
+		Array&	operator=(const Array& cpy)
+		{
+			if (this->array)
+				delete [] this->array;
+			this->array = new T[cpy.len];
+			this->len = cpy.len;
+			for (int i = 0; i < cpy.len; i++)
+				this->array[i] = cpy.array[i];
+			return *this;
+		}
+
+		T&	operator[](const int index)
+		{
+			if (index < 0 || index >= this->len)
+				throw std::out_of_range("\tIndex out of bounds");
+			return this->array[index];
+		}
+
+		int	size()
+		{
+			return (this->len);
+		}
 };
 
 #endif
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -2,6 +2,22 @@
 
 int main()
 {
-	Array<int>	arr(5);
+	Array<int>	arr1(5);
+	//Array<int>	arr2(3);
+	Array<char>	arr2(5);
 
+	for (int i = 0; i < arr2.size(); i++)
+		arr2[i] = i + 'a';
+	for (int i = 0; i < arr1.size(); i++)
+		arr1[i] = i * 2;
+
+	//arr1 = arr2;
+	//Array<int>	arr2(arr1);
+
+	for (int i = 0; i < arr1.size(); i++)
+		std::cout << arr1[i] << " ";
+	std::cout << std::endl;
+	for (int i = 0; i < arr2.size(); i++)
+		std::cout << arr2[i] << " ";
+	std::cout << std::endl;
 }
